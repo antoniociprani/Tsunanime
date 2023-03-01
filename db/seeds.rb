297d53aec7f33e1c6e13681e6a01a707a136b92d
Antonio Ciprani <ciprani.1841305@studenti.uniroma1.it>
2023-03-01 22:54:15 +0100

    Seeds commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+a = User.create(username: "admin", email: "admin@mail.com", password: "adminadmin", roles_mask: 4)
+a.save
+m = User.create(username: "mod", email: "mod@mail.com", password: "modmod", roles_mask: 2)
+m.save
+
+p "Created admin and mod user" 
